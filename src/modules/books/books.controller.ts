73464f5285f62d955862676e10d644c149481f11
igor2000xp <igorring@gmail.com>
2024-09-06 17:33:23 +0200

    feat: create CRUD into books.controller.ts

--- src/modules/books/books.controller.ts
+++ src/modules/books/books.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { BooksService } from './books.service';
 
 @Controller('books')
-export class BooksController {}
+export class BooksController {
+  constructor(private readonly bookService: BooksService) {}
+  @Get()
+  async getAll() {
+    return;
+  }
+
+  @Get(':id')
+  async getBookById(@Param('id') id: string) {
+    return id;
+  }
+
+  @Post()
+  async createBook(@Body() bookDto: any) {
+    return bookDto;
+  }
+
+  @Put(':id')
+  async updateBook(@Param('id') id: string, @Body() bookDto: any) {
+    return bookDto;
+  }
+
+  @Delete(':id')
+  deleteBook(@Param('id') id: string) {
+    return id;
+  }
+}
