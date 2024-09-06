c4084b63c64041b3e92f846dd69c0c629f4395bd
igor2000xp <igorring@gmail.com>
2024-09-06 16:21:16 +0200

    feat: create users: module, controller, service, entity, and repository

---
+++ src/modules/books/books.entity.ts
@@ -0,0 +1,23 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { BaseEntity } from '../../core/entity/base.entity';
+
+@Entity('books')
+export class Book extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  author: string;
+
+  @Column()
+  ageRestriction: number; //возрастные ограничения на книгу
+
+  @Column({ nullable: true })
+  ownerId: number; //id пользователя, который добавил книгу
+
+  @Column({ nullable: true })
+  image?: string;
+}
