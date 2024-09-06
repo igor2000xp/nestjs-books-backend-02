b15a5a27a62fac990b8f663acf0d83a2aee87b7b
igor2000xp <igorring@gmail.com>
2024-09-06 18:54:25 +0200

    feat: add Swagger and cookie-parser

--- src/main.ts
+++ src/main.ts
@@ -11,19 +11,62 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ConfigurationType } from './core/config/configurationType';
-import { ValidationPipe } from '@nestjs/common';
+import { HttpStatus, ValidationPipe } from '@nestjs/common';
+import cookieParser from 'cookie-parser';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   //подключение глобального валидационного pipe https://docs.nestjs.com/techniques/validation
-  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app
+    .useGlobalPipes(
+      new ValidationPipe({
+        forbidNonWhitelisted: true,
+        whitelist: true,
+        transform: true,
+      }),
+    )
+    .use(cookieParser());
 
   //разрешены запросы с любых доменов
   app.enableCors({
     origin: '*', // Разрешает запросы с любых доменов
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Разрешенные методы
     credentials: true, // Включает передачу cookies
+    allowedHeaders: ['Authorization', 'Content-Type'],
+    maxAge: 86400,
+    preflightContinue: false,
+    optionsSuccessStatus: HttpStatus.NO_CONTENT,
+  });
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Description of BD for Books and Auth')
+    .setDescription('Docs for REST API')
+    .setVersion('1.0.0')
+    .addTag('Created by Igor')
+    .addBearerAuth({
+      description: `[just text field] Please enter token in following format: Bearer <JWT>`,
+      name: 'Authorization',
+      bearerFormat: 'Bearer',
+      scheme: 'Bearer',
+      type: 'http',
+      in: 'Header',
+    })
+    .addGlobalParameters({
+      in: 'header',
+      allowEmptyValue: true,
+      required: false,
+      name: 'Content-Language',
+      schema: {
+        enum: ['ru', 'en'],
+      },
+    })
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('docs', app, document, {
+    useGlobalPrefix: true,
+    swaggerOptions: { defaultModelsExpandDepth: -1 },
   });
 
   //получение конфиг сервиса https://docs.nestjs.com/techniques/configuration#using-in-the-maints
