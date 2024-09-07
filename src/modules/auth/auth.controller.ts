f33c464c0cb730362c20d84932f41d1cb5bf81e6
igor2000xp <igorring@gmail.com>
2024-09-07 13:52:54 +0200

    fix: fix local Guard 2

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -9,7 +9,7 @@ import { LocalAuthGuard } from './guards/local-auth.guard';
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @UseGuards(new LocalAuthGuard())
+  @UseGuards(LocalAuthGuard)
   @Post('auth/login')
   async login(@Request() userID: { id: string }) {
     return await this.authService.login(userID.id);
